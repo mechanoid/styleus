d50f9e8e117f3da682dfb6d853fb575e77e2077c
Falk Hoppe <falkhoppe81@googlemail.com>
2012-12-09 18:54:58 +0100

    enabled all toggling options and rendering of ruby code

--- app/helpers/styleus_helper.rb
+++ app/helpers/styleus_helper.rb
@@ -11,27 +11,29 @@ module StyleusHelper
 
   def wrap_component(component)
     article = _styleus_article_wrap(headline: component.headline, id: component.id) do
-      styleus_partials(component.partial_path)
+      styleus_partials(component.partial_path, helper: component.helper?)
     end
     option_bar(component).concat article
   end
 
-  def styleus_partials(partial_path)
-    sample_template = _styleus_representation_wrap(class: '__boxed') do
-      render partial: "#{partial_path}_sample"
-    end
+  def styleus_partials(partial_path, options = { })
+    sample_template = _styleus_representation_wrap(class: '__boxed') { render partial: "#{partial_path}_sample" }
 
-    plain_template = _coderay_highlight_wrap("#{partial_path}.html.erb") do
-      render partial: "#{partial_path}"
-    end
+    plain_template = _html_representation("#{partial_path}.html.erb") { render partial: "#{partial_path}" }
 
-    sample_template.concat(plain_template)
+    helper_template = _helper_representation { render partial: "#{partial_path}_helper" } if options[:helper]
+
+    sample_template.concat(plain_template).concat(helper_template || safe_empty)
   end
 
   def option_bar(component)
     content_tag 'nav', class: '__option_bar' do
       content_tag 'ul' do
-        content_tag('li') { link_to t('icons.html'), component_path(component), class: 'icon', data: { toggle: "##{component.id} [data-subject=html-representation]" } }.concat content_tag('li') { link_to t('icons.helper'), component_path(component), class: 'icon', data: { toggle: 'rails-helper' } }.concat content_tag('li') { link_to t('icons.expand_all'), component_path(component), class: 'icon', data: { toggle: 'rails-helper' } }
+        html_area = content_tag('li') { link_to t('icons.html'), component_path(component), title: t('links.titles.html'), class: 'icon', data: { toggle: "##{component.id} [data-subject=html-representation]" } }
+        helper_area = content_tag('li') { link_to t('icons.helper'), component_path(component), title: t('links.titles.helper'), class: 'icon', data: { toggle: "##{component.id} [data-subject=ruby-representation]" } } if component.helper?
+        presentation_area = content_tag('li') { link_to t('icons.expand_all'), component_path(component), title: t('links.titles.expand_all'), class: 'icon', data: { toggle: "##{component.id} [data-subject*=representation]" } }
+
+        html_area.concat(helper_area || safe_empty).concat(presentation_area)
       end
     end
   end
@@ -53,11 +55,11 @@ module StyleusHelper
     captured_block = capture(&block)
 
     content_tag('article', class: '__sg_article', id: options[:id]) do
-      content  = ''
+      content  = safe_empty
       headline = options[:headline]
       content.concat(content_tag('h3', headline)) if headline
       content.concat(captured_block)
-      content.html_safe
+      content
     end
   end
 
@@ -72,14 +74,23 @@ module StyleusHelper
     end
   end
 
-  def _coderay_highlight_wrap(note = nil, &block)
-    captured_block = capture(&block)
-    code_block     = CodeRay.scan(captured_block.to_s, :html)
 
-    note_tag = note ? content_tag('p', note, class: '__code_note') : ''
+  def _html_representation(note = nil, &block)
+    _coderay_highlight_wrap(note, type: :html, &block)
+  end
+
+  def _helper_representation(&block)
+    _coderay_highlight_wrap(nil, type: :ruby, &block)
+  end
+
+  def _coderay_highlight_wrap(note = nil, options = { type: :html }, &block)
+    captured_block = capture(&block)
+    code_block     = CodeRay.scan(captured_block.to_s, options[:type])
+
+    note_tag = note ? content_tag('p', note, class: '__code_note') : safe_empty
 
     highlighted_code = "#{note_tag}#{code_block.div(:css => :class, line_numbers: :table)}"
-    content_tag('div', data: { subject: 'html-representation' }) do
+    content_tag('div', data: { subject: "#{options[:type]}-representation" }) do
       highlighted_code.html_safe
     end
   end
@@ -88,4 +99,7 @@ module StyleusHelper
     @component_list.join.html_safe
   end
 
+  def safe_empty
+    ''.html_safe
+  end
 end
--- app/models/styleus/view_component.rb
+++ app/models/styleus/view_component.rb
@@ -1,12 +1,16 @@
 module Styleus
   class ViewComponent < Styleus::Base
-    with_attributes :headline, :partial_path
+    with_attributes :headline, :partial_path, :helper
 
     # id is used as anchor id in the anchor menu,
     # so it has to be a uid.
     # TODO: make sure the uid-ness :)
     def id
       headline.underscore.gsub(/ /, '_')
+    end
+
+    def helper?
+      !!helper
     end
 
     class << self
