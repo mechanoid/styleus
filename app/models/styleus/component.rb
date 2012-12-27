764a9c308fd205762185ca4ccafb94ee14b509db
Falk Hoppe <falkhoppe81@googlemail.com>
2012-12-27 09:55:02 +0100

    moving to a model based component registration

--- app/models/styleus/component.rb
+++ app/models/styleus/component.rb
@@ -1,5 +1,48 @@
 module Styleus
   class Component
-    include Styleus::Spacable
+    class_attribute :sections, :components
+
+    class << self
+      def components(*new_components)
+        return registered_components if new_components.empty?
+
+        registered_components.push(*new_components).uniq!
+        registered_components.inspect
+      end
+
+      def registered_components
+        styleus_sections[section_key] ||= []
+      end
+
+      def styleus_sections
+        ::Styleus::Component.sections
+      end
+
+      def sections
+        @sections ||= { }
+      end
+
+      def section_key
+        section_name.to_sym
+      end
+
+      def section_name
+        @section_name ||= to_underscore
+      end
+
+      def to_underscore
+        underscored_module_name
+      end
+
+      private
+
+      def underscored_module_name
+        underscored_name.gsub('/', '_')
+      end
+
+      def underscored_name
+        name.underscore
+      end
+    end
   end
 end
